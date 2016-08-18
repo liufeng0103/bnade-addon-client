e31901b538010493cc3e878b6d228ac0f57afa34
Feng Liu <liufeng0103@163.com>
2016-08-19 00:18:02 +0800

    插件客户端第一版

---
+++ BnadeClient/bnadec.bat
@@ -0,0 +1,15 @@
+@echo off
+set JAVA_EXE=java.exe
+%JAVA_EXE% -version >NUL 2>&1
+if "%ERRORLEVEL%" == "0" goto init
+
+echo.
+echo 错误: 找不到java命令
+echo.
+echo 请安装java 8运行环境，http://www.java.com/
+echo 如果你已安装，请确定把java的bin目录配置到了环境变量PATH下
+pause 
+exit
+
+:init
+java -cp res/* com.bnade.wow.addon.ClientGui
